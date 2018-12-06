8522760731f00779950e1f3cf1833069aa4ed67a
steve <steve@tedu.cn>
2018-12-06 20:06:39 +0800

    p1 of day01

---
+++ python1/day01/p1.py
@@ -0,0 +1,18 @@
+# print("hello world")
+#print("hello","python")
+#print("hello"+" python")
+#print("hello","python",sep="++++")
+#print("hello","python",end="ttttt")
+#uname=input("uname: ")
+#print(uname)
+#num1=int(input("a:"))
+#num2=int(input("b:"))
+#num1=input("a:")
+#num2=input("b:")
+#print(num1+num2)
+if 8 > 5 :
+	print("yes")
+else :
+	print("no")
+print("xixi")
+
