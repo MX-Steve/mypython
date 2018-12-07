eaf029e401c09bd1aec3fb412c16be6da53fc615
steve <steve@tedu.cn>
2018-12-07 14:49:44 +0800

    user and pwd

--- python1/day02/p1.py
+++ python1/day02/p1.py
@@ -1,15 +1,58 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
-py_str="python"
-print(py_str[0])
-print(py_str[-2])
-print(py_str[0:5])
-print(py_str[0:])
-print(py_str[:5])
-print(py_str[::-1])
-print(py_str[::-2])
-print(py_str[::2])
-print(py_str[1::2])
+uname=input("your name:")
+upwd=input("your pwd:")
+if uname == "lisi" and upwd == "123":
+    print("welcome")
+else:
+    print("error")
+# print(0 and 1)
+# print(0 or 1)
+# print(not 1)
+# print(not 0)
+
+# print(1 < 3)
+# print('a'>'b')
+
+
+## adict
+#adict={'name':"bob",'age':28,'school':"tarena"}
+#adict['age']=18
+#print(adict)
+#adict['home']='jiangsu'
+#print(adict)
+#print('name' in adict)
+#print('a' in adict)
+#print('a' not in adict)
+
+# print(adict['name'])
+
+# for i in adict:
+#     print(i)
+
+#atuple can't change
+# atuple=(2,4,5,'tss')
+# print(atuple)
+# print(2 in atuple)
+
+# a=[2,4.5,'ssss',True,False,[0,2,3]]
+# print(a)
+# print(a[-1])
+# print(a[2])
+# a[-1]=5
+# print(a)
+
+# py_str="python"
+# print(py_str[0])
+# print(py_str[-2])
+# print(py_str[0:5])
+# print(py_str[0:])
+# print(py_str[:5])
+# print(py_str[::-1])
+# print(py_str[::-2])
+# print(py_str[::2])
+# print(py_str[1::2])
+
 # #print
 # print("""
 # hi,
