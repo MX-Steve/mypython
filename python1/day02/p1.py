8e405da55b1f8b541179624dec9925026568319c
steve <steve@tedu.cn>
2018-12-07 11:53:07 +0800

    p1 of day02

---
+++ python1/day02/p1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+py_str="python"
+print(py_str[0])
+print(py_str[-2])
+print(py_str[0:5])
+print(py_str[0:])
+print(py_str[:5])
+print(py_str[::-1])
+print(py_str[::-2])
+print(py_str[::2])
+print(py_str[1::2])
+# #print
+# print("""
+# hi,
+# i am lisi
+# i am 16
+# """)
+
+# type num
+# int bool float complex
+# #8
+# print("0o2")
+# print(0o7)
+# #2
+# print(0b1111)
+# #16
+# print(0xa)
+# print(0xe)
+# bool
+# print(True)
+# print(False)
+# print(True == 1)
+# print(False == 0)
+# # fushu
+# print(1+1j)
+
+
+# num=20
+# print(num)
+
+#vars
+# num1=10
+# num2=20
+# print("sum: ", num1+num2)
+
+#user name
+# user=input("user name: ")
+# print("welcome to my site ,",user)
