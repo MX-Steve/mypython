9d6f84a8e8c8ceddc51adeff86d7c43157d0e400
steve <steve@tedu.cn>
2018-12-08 10:28:10 +0800

    day03 am of python1

--- python1/day03/day03.py
+++ python1/day03/day03.py
@@ -2,7 +2,72 @@
 
 # string / list / atuple / adist
 #for
-for ch in 'abcd':
-    print(ch,end='  ')
+# for ch in 'abcd':
+#     print(ch,end='  ')
+
+# alist={'name':"tom","age":28,"school":"tarena"}
+# for i in alist:
+#     print(i,alist[i])
+# print(i)
+#
+# for i in [1,2,3,4,5]:
+#     print(i,end="  ")
+
+# for i in range(1,101):
+#     if i % 10 != 0:
+#         print(i,end="  ")
+#     else:
+#         print(i)
+
+fibs=[0,1]
+# fibs.append(1)
+# fibs.append(2)
+# fibs.append(3)
+# fibs.append(4)
+# fibs.append(5)
+# print(fibs)
+# n=int(input("num:"))
+# for i in range(n-2):
+#     fibs.append(fibs[-1]+fibs[-2])
+# print(fibs)
+
+# # 99
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print(j,"*",i,"=",j*i,end="  ")
+#     print()
+
+# sum=0
+# for i in range(1,101):
+#     sum+=i
+# print(sum)
+
+# print(list(range(1,101,2)))
+# print(list(range(100,0,-2)))
+
+# # 99
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print("%dx%d=%d"%(j,i,j*i),end="  ")
+#     print()
+
+# list=[]
+# for i in range(1,21):
+#     list.append(i)
+# print(list)
+# # liebiaojiexi
+# print([i for i in range(1,21)])
+# print([i**2 for i in range(1,21)])
+# print([i for i in range(1,21,2)])
+# print([i for i in range(2,21,2)])
+
+# # open a file
+# fangwenmoshi
+# r: w: a:
+#r
+# fobj=open('/root/a.cnf')
+# print(fobj)
+
+fobj=open('hello.py','w')
 
 print()
