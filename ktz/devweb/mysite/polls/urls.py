54e7c8daf7d41d7696d81d9bb5134f7f871101b8
mx-steve <2513556525@qq.com>
2019-08-14 16:53:48 +0800

    polls-1

--- ktz/devweb/mysite/polls/urls.py
+++ ktz/devweb/mysite/polls/urls.py
@@ -4,6 +4,6 @@ from . import  views
 
 urlpatterns=[
     url(r'^$', views.index,name='index'),
-    url(r'^1$',views.details,name='details'),
-    url(r'result$',views.result,name='result'),
+    url(r'^(\d+)/$',views.details,name='details'),
+    url(r'^(\d+)/result$',views.result,name='result'),
 ]
--- ktz/devweb/mysite/polls/views.py
+++ ktz/devweb/mysite/polls/views.py
@@ -6,8 +6,8 @@ def index(request):
     # return HttpResponse('<h1>polls index</h1>')
     return  render(request,'index.html')
 
-def details(request):
-    return render(request,'details.html')
+def details(request,question_id):
+    return render(request,'details.html',{'question_id':question_id})
 
-def result(request):
-    return render(request,'result.html')
+def result(request,question_id):
+    return render(request,'result.html',{'question_id':question_id})
